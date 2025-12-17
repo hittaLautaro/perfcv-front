67f09a67aa8f1d9991ed28fc87a096f61fc9634b
hittaLautaro <hitta.lautaro@gmail.com>
2025-12-17 10:35:49 -0300

    added footer to main layout and removed it from landing page

--- src/Landing/page.jsx
+++ src/Landing/page.jsx
@@ -11,10 +11,8 @@ const LandingPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen bg-zinc-950">
-      
+    <div className="flex flex-col bg-zinc-950">
       <Hero id="hero" isVisible={isVisible} />
-      <Footer />
     </div>
   );
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../global/components/Header";
+import Footer from "../global/components/Footer";
 
 const MainLayout = () => {
     return (
         <div className="flex flex-col min-h-screen bg-zinc-950">
             <Header />
             <Outlet />
+            <Footer />
         </div>
     );
 };
